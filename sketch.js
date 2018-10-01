c7b38ea929a984969e12af69b7e489a2e0bb4143
Samanimkr <Samanimkr@Outlook.com>
2018-10-01 22:16:13 +0100

    snake movements

--- sketch.js
+++ sketch.js
@@ -1,12 +1,42 @@
 var s;
+var scl = 20;
+var food;
 
 function setup(){
   createCanvas(600,600);
-  // s = new Snake();
+  s = new Snake();
+  frameRate(15);
+  pickLocation();
+}
+
+function pickLocation(){
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
 }
 
 function draw(){
   background(51);
-  // s.update();
-  // s.show();
+  s.update();
+  s.show();
+
+  if (s.eat(food)){
+    pickLocation();
+  }
+
+  fill(255,0,100);
+  rect(food.x, food.y, scl, scl);
+}
+
+function keyPressed(){
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
 }
--- snake.js
+++ snake.js
@@ -1,3 +1,44 @@
 function Snake(){
-  
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
+
+  this.dir = function(x, y){
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+
+  this.eat = function(pos){
+    var d = dist(this.x, this.y, pos.x, pos.y);
+    if (d < 1){
+      this.total++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  this.update = function(){
+    for (var i = 0; i < this.total-1; i++) {
+      this.tail[i] = this.tail[i+1];
+    }
+    this.tail[this.total-1] = createVector(this.x, this.y);
+
+    this.x += this.xspeed*scl;
+    this.y += this.yspeed*scl;
+
+    this.x = constrain(this.x, 0, width-scl);
+    this.y = constrain(this.y, 0, width-scl);
+  }
+
+  this.show = function(){
+    fill(255);
+    for (var i = 0; i < this.total; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    }
+    rect(this.x, this.y, scl, scl);
+  }
 }
